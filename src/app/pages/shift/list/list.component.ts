9595082c012ed79324cca178b3198d24998a15e4
JazminZapata <yaszapata4@gmail.com>
2025-11-22 18:40:22 -0500

    Ids y view motorcycle

--- src/app/pages/shift/list/list.component.ts
+++ src/app/pages/shift/list/list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Shift } from 'src/app/models/Shift';
 import { ShiftService } from 'src/app/services/shift.service';
-import { DriverService } from 'src/app/services/driver.service';
-import { MotorcycleService } from 'src/app/services/motorcycle.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -13,29 +11,11 @@ import Swal from 'sweetalert2';
 export class ListComponent implements OnInit {
   shifts: Shift[] = [];
 
-  constructor(
-    private service: ShiftService,
-    private driverService: DriverService,
-    private motorcycleService: MotorcycleService
-  ) { }
+  constructor(private service: ShiftService) { }
 
   ngOnInit(): void {
     this.service.list().subscribe(data => {
       this.shifts = data;
-      
-      // Cargar información de drivers y motorcycles para cada shift
-      this.shifts.forEach(shift => {
-        if (shift.driver_id) {
-          this.driverService.view(shift.driver_id).subscribe(driver => {
-            shift.driver = driver;
-          });
-        }
-        if (shift.motorcycle_id) {
-          this.motorcycleService.view(shift.motorcycle_id).subscribe(motorcycle => {
-            shift.motorcycle = motorcycle;
-          });
-        }
-      });
     });
   }
 
