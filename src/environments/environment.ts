161a11ac5f16038e7ed4f4204f8bd9ae985574c7
Aleandro Duque <josealejandrodm24@gmail.com>
2025-11-28 11:40:13 -0500

    Enhance Microsoft login to retrieve user profile photo and update session handling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   url_ms_security :'https://f2fe41f8-0662-4d7b-8f9e-c4e051483e4c.mock.pstmn.io',
   url_web_socket: 'http://127.0.0.1:5000',
   url_mock_charts: 'https://a571fe5a-c44a-42b8-91be-a4f31b91d9e8.mock.pstmn.io',
-  gemini_api_key: 'AIzaSyA5yx0HPDF77lltHcvIbIZIxjnqRb590KU'
+  gemini_api_key: 'AIzaSyBj61AcHRtmn6kwbw314vPeNMgCufAzkis'
 };
 
 /*
